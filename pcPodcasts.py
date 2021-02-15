b35ad70e25da2128e1d4e3dc3b974a9075a7a079
rkcastelino <rkcastelino@gmail.com>
2021-02-15 00:11:26 -0800

    got working though there are bugs to work out

--- pcPodcasts.py
+++ pcPodcasts.py
@@ -25,6 +25,7 @@
     # set up local git repo
     # add images/gifs to documentation
     # had animation issue with "[No Ads] Ep. 15 - 8 Simple Rules for Dadding my Teenage Paeden" since exceeded line limit, wrote to new line everytime, will probably now need to use curses
+    # aknowledging going to quit, more immediate quit, neater command line, only detect input from specific terminal
 
 import requests
 import pdb
@@ -32,6 +33,7 @@ import os
 import threading, queue
 from bs4 import BeautifulSoup as Soup
 import time
+import keyboard
 
 def setup():
     # Get rss-xml file from Pat        reon link
@@ -54,64 +56,68 @@ def setup():
     return item_list, files
 
 def monitor():
-    x = 3
+    while True:  # making a loop
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed('q'):  # if key 'q' is pressed
+                quit_queue.put('quit')
+                break  # finishing the loop
+        except:
+            break  # if user pressed a key other than the given key the loop will break
+
 
 
 def downloading(item_list, files):
+    quit_flag = 0;
+
     for item in item_list:
+        try:
+            quit_flag = quit_queue.get(False)
+        except queue.Empty:
+            title = item.title.text + '.mp3'
+            # Test if title has characters which break file naming
+            if title.find('/') | title.find('"'):
+                title = title.replace('/', ',')
+                title = title.replace('"', "'")
 
-        title = item.title.text + '.mp3'
-        # Test if title has characters which break file naming
-        if title.find('/') | title.find('"'):
-            title = title.replace('/', ',')
-            title = title.replace('"', "'")
+            # Skip file if already downloaded
+            if title in files:
+                 continue
 
-        # Skip file if already downloaded
-        if title in files:
-             continue
-
-        # Get specific url, download and save mp3
-        mp3_url = item.enclosure.get('url')
-        title_queue.put(title[:-4])
-        mp3 = requests.get(mp3_url)
-        title_queue.put('done')
-        with open('Episodes/' + title, 'wb') as f:
-            f.write(mp3.content)
+            # Get specific url, download and save mp3
+            mp3_url = item.enclosure.get('url')
+            title_queue.put(title[:-4])
+            mp3 = requests.get(mp3_url)
+            with open('Episodes/' + title, 'wb') as f:
+                f.write(mp3.content)
+        if quit_flag == 'quit':
+            break
     title_queue.put('all done')
 
 def downloadingAnimation():
     animation = ["      ", " .    ", " . .  ", " . . ."]
     idx = 0
-    title = 'none'
+    one_cycle_done = 0
 
     while True:
         try:
             title = title_queue.get(False)
-            if title == 'done':
-                print('Downloaded "' + title + '"       ', flush=True)              # Need spaces at end to overwite periods
+
+            if one_cycle_done == 1:
+                print('Downloaded "' + old_title + '"       ', flush=True)              # Need spaces at end to overwite periods
                 idx = 0
-                continue
+
             if title=='all done':
                 break
 
             print('Downloading "' + title + '"' + animation[idx % len(animation)], end="\r", flush=True)        # Flush is required since using GitBash as terminal
         except queue.Empty:
+            old_title = title
             print('Downloading "' + title + '"' + animation[idx % len(animation)], end="\r", flush=True)        # Flush is required since using GitBash as terminal
             idx += 1
             time.sleep(1)
+
+            one_cycle_done = 1
             pass
-
-
-
-
-        # if title != "none":
-        #     print('here')
-        #     print('Downloading "' + title + '"' + animation[idx % len(animation)], end="\r", flush=True)        # Flush is required since using GitBash as terminal
-        #     idx += 1
-        #     time.sleep(1)
-
-#make sure to reset idx with new title as well
-
 
 def main():
     item_list, files = setup()
@@ -120,9 +126,11 @@ def main():
     downloadingThread = threading.Thread(target = downloading, args=(item_list, files))
     downloadingAnimationThread = threading.Thread(target = downloadingAnimation)
 
+    monitorThread.start()
     downloadingThread.start()
     downloadingAnimationThread.start()
 
 if __name__ == "__main__":
     title_queue = queue.Queue()
+    quit_queue = queue.Queue()
     main()
